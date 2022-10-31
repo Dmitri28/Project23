bc025ae698fadd9029464d0413c156ab81ce544e
Dima <117075948+Dmitri28@users.noreply.github.com>
2022-10-31 13:22:36 +0200

    Project23
    Project23

---
+++ src/square1/Square1.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package square1;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author admin
+ */
+public class Square1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+     
+     double a= in.nextDouble();
+     double b= in.nextDouble();
+     double c=in.nextDouble();
+     
+    double P=a+b+c;
+    System.out.println(P);
+
+  
+
+    
+}
+
+   
+}
